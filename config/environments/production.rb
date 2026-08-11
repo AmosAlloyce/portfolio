389f51be0ac5c6e161cb6f4269e863db06c425d9
AmosAlloyce <alloyceoloo5@gmail.com>
2026-08-11 12:17:57 +0300

    Add Tailwind CSS to asset precompilation list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
+  # Precompile additional assets
+  config.assets.precompile += %w( tailwind/application.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
